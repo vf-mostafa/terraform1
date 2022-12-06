331702820c9f2077ed29364b3facabdd158c07bf
vf-mostafa <98397927+vf-mostafa@users.noreply.github.com>
2022-12-06 12:32:45 +0200

    init tf

---
+++ init.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.35.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "ASIAWWTSZAEJ5DZL5XEG"
+  secret_key = "iRU/IssrJRtjCn9obOrMgqf2wlqivwCQYEHXlZNw"
+  token = "FwoGZXIvYXdzEPr//////////wEaDHBnZF3KUZ87+LHMvCK8AePYHLVESV0HVcEyFUMrtKZ/dOvtgkFDv3imDL2jcTnJBK93OyorQTb3/FZTezXmskC7rzhf2exDbpK8iMi8Um7PwbYCXMgoQ08lkoJUING7gXlChrCJ1q7zCvThIqqCDfmJJpkc3Pwb9HaEAziUUP+4A7Q3Y1sfM/dhtgUd1t07tvJklFgpkXXEvli8xodlil25YQBFIhqZl8yZt/rmsfnZcc4FGh4+RNsVOTQjkM6ihk1yX+/+qu9rtg2pKNf7u5wGMi03YOokX4mEZxcrRiTTSGr6X/Jn0Xmx00f7rbUzzyP/jZ4v3ajSmf0Q0LVLcdo="
+}
